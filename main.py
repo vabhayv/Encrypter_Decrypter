98b4b4ca9887ea8f479dfc481cf3714a10c2c653
Abhay Lodhi <156104058+vabhayv@users.noreply.github.com>
2024-01-18 00:45:46 +0530

    Update main.py
    I updated it 😮‍💨

--- main.py
+++ main.py
@@ -11,33 +11,41 @@ availabe_symbols = [' ', '!', '@', '#', '$', '%', '&', '*', '(', ')', '_',
 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 
 'U', 'V', 'W', 'X', 'Y', 'Z' ]
 
-direction = input("Type 'encode' to Encrypt or Type 'decode' to Decrypt: \n")
-text = input("Type your message: \n")
-shift = int(input("Type your code number: \n"))
 
-def encrypt_decrypt(user_text,shift_no,choosen_direction):
-    shifted_text = ""
-    for letter in user_text:
-        position = availabe_symbols.index(letter)
-        if choosen_direction == "encode":
-            new_position = position + shift_no
-        elif choosen_direction == "decode":
-            new_position = position - shift_no
-        shifted_text += availabe_symbols[new_position]
-    print(f"The {choosen_direction}d text is {shifted_text}")
-    
-    
-def decrypt(decoded_text,shift_no):
-    normal_text = ""
-    for letter in decoded_text:
-        position = availabe_symbols.index(letter)
-        new_position = position - shift_no
-        normal_text += availabe_symbols[new_position]
-    print(f"The normal text is {normal_text}")
+wanna_try_again = True
 
-
-encrypt_decrypt(user_text=text, shift_no=shift,choosen_direction=direction)  
+while wanna_try_again:
+    direction = input("Type 'encode' to Encrypt or Type 'decode' to Decrypt: \n")
+    text = input("Type your message: \n")
+    shift = int(input("Type your code number: \n"))
     
+    def encrypt_decrypt(user_text,shift_no,choosen_direction):
+        shifted_text = ""
+        for letter in user_text:
+            position = availabe_symbols.index(letter)
+            if choosen_direction == "encode":
+                new_position = position + shift_no
+            elif choosen_direction == "decode":
+                new_position = position - shift_no
+            else :
+                print("\n***PLEASE SELECT THE CORRECT OPERATION***\n")
+            shifted_text += availabe_symbols[new_position]
+        print(f"The {choosen_direction}d text is: \n{shifted_text} \n  ")
+    
+    shift = shift%62
+    
+    encrypt_decrypt(user_text=text, shift_no=shift,choosen_direction=direction)  
+    
+    user_choice = input("Wanna try again?\nyes or no (default is no) : ")
+    if user_choice == "yes" :
+        wanna_try_again = True
+        print("Here....\n")
+    elif user_choice == "no" :
+        wanna_try_again = False
+        print("I hope you enjoyed it, visit again :)")
+    else:
+        wanna_try_again = False
+        print("I hope you enjoyed it :)")
 
 
 
