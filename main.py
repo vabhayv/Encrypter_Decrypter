d30b34a136c2c6012dcf06b300794daf789aadb8
Abhay Lodhi <156104058+vabhayv@users.noreply.github.com>
2024-01-17 23:57:12 +0530

    Update main.py
    updated the code, but not finished yet :)

--- main.py
+++ main.py
@@ -1,41 +1,47 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
-'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+availabe_symbols = [' ', '!', '@', '#', '$', '%', '&', '*', '(', ')', '_',
+'+', '-', '/', '?', '.', ':', ';', '[', ']', '{', '}', '|', ',', '`', '~',
+'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f',
+'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 
+'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ', '!',
+'@', '#', '$', '%', '&', '*', '(', ')', '_', '+', '-', '/', '?', '.', ':', ';', 
+'[', ']', '{', '}', '|', ',', '`', '~', '0', '1', '2', '3', '4', '5', '6', '7', 
+'8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 
+'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 
+'U', 'V', 'W', 'X', 'Y', 'Z' ]
 
-wanna_try_again = True
-while wanna_try_again:
-    direction = input("Type 'encode' to Encrypt and Type 'decode' to Decrypt: \n")
-    text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift code:\n"))
-       
-    def encrypt_decrypt(plain_text, shift_amount):
-        user_text = ""
-        for letter in plain_text:
-            position = alphabet.index(letter)
-            if direction == 'encode':
-                new_position = position + shift_amount
-            elif direction == 'decode':
-                new_position = position - shift_amount
-            new_letter = alphabet[new_position]
-            user_text += new_letter
-        if direction == 'encode':
-            print(f"The encoded text is {user_text}")
-        elif direction == 'decode':
-            print(f"The decoded text is {user_text}")
-            
-                
-        
-    
-    encrypt_decrypt(plain_text=text, shift_amount=shift)
-    say = input("wanna use again? type 'yes' to continue or 'no' to stop")
-    if say == 'no':
-        print("hope you enjoy it :)")
-        wanna_try_again = False
-    elif say == 'yes':
-        print("here you go !!")
-        wanna_try_again = True
+direction = input("Type 'encode' to Encrypt or Type 'decode' to Decrypt: \n")
+text = input("Type your message: \n")
+shift = int(input("Type your code number: \n"))
+
+def encrypt_decrypt(user_text,shift_no,choosen_direction):
+    shifted_text = ""
+    for letter in user_text:
+        position = availabe_symbols.index(letter)
+        if choosen_direction == "encode":
+            new_position = position + shift_no
+        elif choosen_direction == "decode":
+            new_position = position - shift_no
+        shifted_text += availabe_symbols[new_position]
+    print(f"The {choosen_direction}d text is {shifted_text}")
     
     
+def decrypt(decoded_text,shift_no):
+    normal_text = ""
+    for letter in decoded_text:
+        position = availabe_symbols.index(letter)
+        new_position = position - shift_no
+        normal_text += availabe_symbols[new_position]
+    print(f"The normal text is {normal_text}")
+
+
+encrypt_decrypt(user_text=text, shift_no=shift,choosen_direction=direction)  
     
-    
+
+
+
+
+
+
+
